11f9e7bbd97a8204d3c1589ce442dee6e82caf72
Emirhan Dogandemir <emirhandgndmr51@gmail.com>
2021-08-23 10:36:58 +0300

    Admin Page sayfa düzeni oluşturuldu

--- bookstore/src/pages/admin/AdminPage.jsx
+++ bookstore/src/pages/admin/AdminPage.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 
 export default function AdminPage() {
-  return <div>Adminssss</div>;
+  return (
+    <div className="container-fluid">
+      <p className="fs-3"> Admin Paneli</p>
+      <hr />
+      <div className="row">
+        <div className="col-3 bg-secondary ">
+          <div className="row p-2"> Edit Categories</div>
+          <div className="row p-2"> Edit Books</div>
+          <div className="row p-2"> Edit Publishments</div>
+          <div className="row p-2"> Edit Authors</div>
+          <div className="row p-2"> Edit Users</div>
+          <div className="row p-2"> Edit Customers</div>
+        </div>
+        <div className="col-9"></div>
+      </div>
+    </div>
+  );
 }
